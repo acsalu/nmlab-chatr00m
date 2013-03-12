351ed1d1b16237f2fc0703eee994a2dea4ad735d
Acsa Lu <acsa0210@gmail.com>
2013-03-12 22:25:27 +0800

    use OO to rewrite server.py

--- server/server.py
+++ server/server.py
@@ -3,8 +3,12 @@ import select
 import sys
 import threading
 
+HOST = ''
+PORT = 10627
+
 class Client(threading.Thread):
     def __init__(self, conn, addr):
+        print("client from " + addr[0] + " is connected...")
         self.conn = conn
         self.addr = addr
         self.size = 1024
@@ -14,36 +18,28 @@ class Client(threading.Thread):
         while running:
             data = self.conn.recv(self.size)
             if data:
-                print(addr[0]+ ": " + data.decode('UTF-8'))
-            else:
-                self.conn.close()
-                runnging = 0
+                print(self.addr[0]+ ": " + data.decode('UTF-8'))
+            #else:
+            #    self.conn.close()
+            #    runnging = 0
 
 
-host = ''
-port = 10627
+class Server:
+	def __init__(self):
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#self.s.setblocking(0)
+		PORT = input('PORT = ')
+	
+	def serve(self):
+		self.s.bind((HOST, PORT))
+		self.s.listen(1)
+		while True:
+			conn, addr = self.s.accept()
+			c = Client(conn, addr)
+			c.run()
+		conn.close()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
-s.listen(1)
-while True:
-    conn, addr = s.accept()
-    Client(conn, addr).run()
-    #data = conn.recv(1024)
-    #if not data: break
-    #conn.sendall(data)
-    #print(addr[0] + ": " + data.decode('UTF-8'))
-conn.close()
 
-"""
-try:
-    s.connect((host, port))
-    s.shutdown(2)
-    print("Success connecting to ")
-    print(host, " on port: ", str(port))
-except socket.error as e:
-    print("Cannot connect to ")
-    print(host, " on port: ", str(port))
-    print(e)
-"""
-
+if __name__ == '__main__':
+	server = Server()
+	server.serve()
