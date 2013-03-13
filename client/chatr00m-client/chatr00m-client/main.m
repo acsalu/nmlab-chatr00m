9646e749896dff6cf11381f0dc862b0890fd514f
Acsa Lu <acsalu@Acsa-Lus-MacBook-Pro.local>
2013-03-13 10:29:34 +0800

    client project created

---
+++ client/chatr00m-client/chatr00m-client/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  chatr00m-client
+//
+//  Created by Acsa Lu on 3/13/13.
+//  Copyright (c) 2013 com.nmlabg7. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
