cf5cf10356dd0eef72028a98eaff72e5e5c3b337
KeyRe <Li.KeyRe@gmail.com>
2013-03-19 22:31:16 +0800

    add class definition

--- server/server.py
+++ server/server.py
@@ -6,18 +6,30 @@ import sys
 import codecs
 import signal
 import json
+import queue
 
-HOST = ''
+HOST = ""
 PORT = 10627
 
-ACTION_TALK = 'TALK'
+room_ID_max = 0
+ACTION_TALK = "TALK"
+
+# class Client:
+#     def __init__(self, s, name):
+#         self.s = s
+#         self.name = name
+
+# class Room:
+#     def __init__(self, ):
+#         pass
+
 
 class Server:
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.clients = 0
-        self.clientmap = {}
+        self.client_map = {}
         self.outputs = []
         self.s.bind((HOST, PORT))
         print ("Start listen to port " + str(PORT))
@@ -25,6 +37,13 @@ class Server:
         
         signal.signal(signal.SIGINT, self.sighandler)
         
+        self.message_queues = {}
+        self.message_queues[self.s] = queue.Queue()
+        # key   : int (0 is lobby) 
+        # value : list of sockets
+        self.room_list = {}
+        self.room_list[0] = [self.s]
+
 
     def sighandler(self, signum, frame):
         #Close the server
@@ -34,9 +53,12 @@ class Server:
         self.s.close()    
     
     def getname(self, client):
-        info = self.clientmap[client]
+        info = self.client_map[client]
         host, name = info[0][0], info[1]
-        return '@'.join((name, host))
+        return "@".join((name, host))
+
+    def add():
+        pass
     
     def serve(self):
         inputs = [self.s, sys.stdin]
@@ -57,17 +79,17 @@ class Server:
                     client, address = self.s.accept()
                     print ("server: Got connection %d from %s" % (client.fileno(), address))
                     # read client login name
-                    #cname = receive(client).split('NAME: ')[1]
+                    #cname = receive(client).split("NAME: ")[1]
                     
                     
                     self.clients += 1
-                    client.send(('CLIENT: ' + str(address[0])).encode('utf-8'))
+                    client.send(("CLIENT: " + str(address[0])).encode("utf-8"))
                     inputs.append(client)
                     cname = address[0]
-                    self.clientmap[client] = (address, cname)
+                    self.client_map[client] = (address, cname)
                     
                     for o in self.outputs:
-                        o.send(("\n(Connected: New client %d from %s)" % (self.clients, self.getname(client))).encode('utf-8'))
+                        o.send(("\n(Connected: New client %d from %s)" % (self.clients, self.getname(client))).encode("utf-8"))
                     
                     self.outputs.append(client)
                     
@@ -81,37 +103,29 @@ class Server:
                     try:
                         data = s.recv(1024)
                         if data:
-                            print("[" + self.getname(s) + "] " + data.decode('UTF-8'))
-                            data = data.split(b'\0',1)[0]
-                            msg = json.loads(data.decode('UTF-8'))
+                            print("[" + self.getname(s) + "] " + data.decode("UTF-8"))
+                            data = data.split(b"\0",1)[0]
+                            msg = json.loads(data.decode("UTF-8"))
                             
-                            if msg['action'] == ACTION_TALK:
+                            if msg["action"] == ACTION_TALK:
                                 for o in self.outputs:
                                     if o != self.s:
-                                       o.send((self.getname(s) + ":" + msg['content']).encode('UTF-8'))
+                                       o.send((self.getname(s) + ":" + msg["content"]).encode("UTF-8"))
                         else:
-                            print ('server: %d hung up' % s.fileno())
+                            print ("server: %d hung up" % s.fileno())
                             self.clients -= 1
                             s.close()
                             inputs.remove(s)
                             self.outputs.remove(s)
                             
-                            msg = '\n(Hung up: Client from %s' % self.getname(s)
+                            msg = "\n(Hung up: Client from %s" % self.getname(s)
                             for o in self.outputs:
-                                o.send(msg.encode('utf-8'))
+                                o.send(msg.encode("utf-8"))
                     except socket.error as e:
                         inputs.remove(s)
                         self.outputs.remove(s)
-                    
-        """
-        conn, addr = self.s.accept()
-        conn.send('Welcome to server\n'.encode('utf-8'))
-        c = Client(conn, addr)
-        c.run()
-        """
-        #conn.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     server = Server()
     server.serve()
